912bd0180561bd7f6bcc1f5d1ed4d6b5c33f7fa1
Daniel Hawn <danieljhawn@gmail.com>
2020-03-15 14:23:56 -0500

    fixes about and contact page with header and footer

--- pages/About.js
+++ pages/About.js
@@ -1,14 +1,24 @@
 // import StickerCard from '../components/StickerCard'
 // import TshirtCard from '../components/TshirtCard'
+import Head from 'next/head'
+import Link from 'next/link';
+import MyLayout from '../components/MyLayout';
 
 function About() {
-    return (
-        <div>
-            <p>Union Print shop, Austin Pride, blah blah blah</p>
-            <p>What We Do</p>
-            {/* <StickerCard /><TshirtCard /> */}
-        </div>
-    )
+    return (<>
+        <Head>
+            <title>Project Nighthawk</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+        </Head>
+        <MyLayout>
+            <div>
+                <p>Union Print shop, Austin Pride, blah blah blah</p>
+                <p>What We Do</p>
+                {/* <StickerCard /><TshirtCard /> */}
+            </div>
+        </MyLayout>
+    </>)
 }
 
 export default About
--- pages/Contact.js
+++ pages/Contact.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link';
+import MyLayout from '../components/MyLayout';
+
 
 function Contact() {
-    return (
-        <div>
-            Phone Number. Email
-        </div>
-    )
+    return (<>
+        <Head>
+            <title>Project Nighthawk</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+        </Head>
+        <MyLayout>
+            <div>
+                <p>Phone Number</p>
+                <p>Email address</p>
+                {/* <StickerCard /><TshirtCard /> */}
+            </div>
+        </MyLayout>
+    </>)
 }
 
 export default Contact
