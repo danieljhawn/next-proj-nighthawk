3b3f69b00257394c122c2e14fc141860858ba33a
Daniel Hawn <danieljhawn@gmail.com>
2020-03-15 19:51:28 -0500

    last changes of the night

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,18 @@ import { Navbar, Nav } from "react-bootstrap"
 
 const layoutStyle = {
   fontFamily: 'Arial, Helvetica, sans-serif',
+  fontSize: '15px',
   color: '#FFFFFF',
   textDecoration: 'none',
   fontStyle: 'normal',
   textTransform: 'uppercase',
-} 
+  width: '100%'
+}
 
 const linkStyle = {
   fontFamily: 'Arial, Helvetica, sans-serif',
   fontSize: '15px',
-  letterSpacing:  '7px',
+  letterSpacing: '7px',
   wordSpacing: '2px',
   color: '#FFFFFF',
   textDecoration: 'none',
@@ -23,14 +25,13 @@ const linkStyle = {
 const Header = () => (
   <div style={layoutStyle}>
     <Navbar fixed="top" bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/pricing">Pricing</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-        </Nav>
-        <Nav.Link href="/Signup">Sign Up</Nav.Link> | <Nav.Link href="/Login">Log In</Nav.Link>
-      </Navbar>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/pricing">Pricing</Nav.Link>
+      </Nav>
+      <Nav.Link href="/Signup">Sign Up</Nav.Link> | <Nav.Link href="/Login">Log In</Nav.Link>
+    </Navbar>
   </div>
 );
 
