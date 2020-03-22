c78af2cccb746fff17b2f868429c44230fc50e16
Daniel Hawn <danieljhawn@gmail.com>
2020-03-22 12:09:43 -0500

    changes spelling and capitalization stuff

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import user from './User';
-import stickerJob from './stickerjob';
+import stickerJob from './stickerJob';
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
