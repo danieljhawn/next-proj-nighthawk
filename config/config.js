0d09ea80a44ac67a8b2d034765230b331eef5bf2
Daniel Hawn <danieljhawn@gmail.com>
2020-04-07 21:36:27 -0500

    testing...

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
-{
+module.exports = {
     "development": {
         "host": "127.0.0.1",
         "dialect": "mysql",
+        "dialectModule": require('mysql2'),
         "operatorsAliases": false
     },
     "test": {
@@ -10,14 +11,17 @@
         "database": "nodejs_login1",
         "host": "127.0.0.1",
         "dialect": "mysql",
+        "dialectModule": require('mysql2'),
         "operatorsAliases": false
     },
     "production": {
-        "username": "root",
-        "password": null,
+        "username": "f8xjt6wjqcl7d9pv",
+        "password": "wb53k5q3n8eg3kgw",
         "database": "database_production",
-        "host": "127.0.0.1",
+        "host": "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         "dialect": "mysql",
+        "dialectModule": require('mysql2'),
         "operatorsAliases": false
     }
 }
+
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ import stickerjob from './Stickerjob';
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 
 let sequelize;
 if (config.use_env_variable) {
