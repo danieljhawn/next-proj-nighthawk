c1e43d599123b7a27917d4190e08c9a3dbaed4ce
Daniel Hawn <danieljhawn@gmail.com>
2020-04-14 19:27:01 -0500

    deploying again

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
 module.exports = {
     "development": {
-        "username": "root",
-        "password": "TR!nity9812",
         "database": "nodejs_login1",
         "host": "127.0.0.1",
         "dialect": "mysql",
@@ -15,8 +13,6 @@ module.exports = {
         "operatorsAliases": false
     },
     "production": {
-        "username": "f8xjt6wjqcl7d9pv",
-        "password": "wb53k5q3n8eg3kgw",
         "database": "ylx7dx2klw0gouq3",
         "host": "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         "dialect": "mysql",
